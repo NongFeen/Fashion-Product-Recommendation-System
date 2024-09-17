55c9557aaaa3afaa533222dbaf7145547a3c2c40
NongFeen <kakfeenza@gmail.com>
2024-09-18 00:26:58 +0700

    ??? why this is not get ignore?

--- test.py
+++ test.py
@@ -1,70 +1,14 @@
-import streamlit as st
-import os
-from PIL import Image
-import numpy as np
-import pickle
-import tensorflow
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.layers import GlobalMaxPooling2D
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
-from sklearn.neighbors import NearestNeighbors
-from numpy.linalg import norm
+import pandas as pd
 
-# Load feature list and filenames
-feature_list = np.array(pickle.load(open('embeddings.pkl', 'rb')))
-filenames = pickle.load(open('filenames.pkl', 'rb'))
+# ระบุ path ของไฟล์ CSV
+csv_path = 'dataset/fix_styles.csv'
 
-# Initialize the ResNet50 model
-model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-model.trainable = False
-model = tensorflow.keras.Sequential([
-    model,
-    GlobalMaxPooling2D()
-])
+# อ่านไฟล์ CSV
+df = pd.read_csv(csv_path, delimiter=';')
 
-st.title('Fashion Recommender System')
+# แสดงข้อมูล 5 แถวแรก เพื่อดูโครงสร้างของข้อมูล
+print(df.head())
 
-def save_uploaded_file(uploaded_file):
-    try:
-        with open(os.path.join('uploads', uploaded_file.name), 'wb') as f:
-            f.write(uploaded_file.getbuffer())
-        return 1
-    except:
-        return 0
+# เข้าถึงข้อมูลของ product 44065 (สมมติว่า product ID อยู่ในคอลัมน์แรก)
 
-def feature_extraction(img_path, model):
-    img = image.load_img(img_path, target_size=(224, 224))
-    img_array = image.img_to_array(img)
-    expanded_img_array = np.expand_dims(img_array, axis=0)
-    preprocessed_img = preprocess_input(expanded_img_array)
-    result = model.predict(preprocessed_img).flatten()
-    normalized_result = result / norm(result)
-    return normalized_result
 
-def recommend(features, feature_list):
-    neighbors = NearestNeighbors(n_neighbors=10, algorithm='brute', metric='euclidean')
-    neighbors.fit(feature_list)
-    distances, indices = neighbors.kneighbors([features])
-    return distances, indices
-
-# File upload
-uploaded_file = st.file_uploader("Choose an image")
-if uploaded_file is not None:
-    if save_uploaded_file(uploaded_file):
-        # Display the file
-        display_image = Image.open(uploaded_file)
-        st.image(display_image)
-        
-        # Feature extraction
-        features = feature_extraction(os.path.join("uploads", uploaded_file.name), model)
-        
-        # Recommendation
-        distances, indices = recommend(features, feature_list)
-        
-        # Display recommendations and scores
-        cols = st.columns(10)
-        for i in range(10):
-            with cols[i]:
-                st.image(filenames[indices[0][i]])
-    else:
-        st.header("Some error occurred in file upload")
